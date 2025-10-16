cc4fce52041b15bdfab6578f01f2108757e976c2
James <james@JamesdeMacBook-Pro.local>
2025-10-16 16:40:38 +0800

    Add simple test page

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">測試頁面</h1>
+      <p>如果您看到這個頁面，表示 Vercel 部署正常。</p>
+    </div>
+  );
+}
